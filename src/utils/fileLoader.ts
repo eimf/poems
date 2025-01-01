6184e5e53ea0523a0daa18d811c94afb5160f424
Zeke <lzdzel@gmail.com>
2024-12-31 23:22:14 -0600

    fix deployment issues

--- src/utils/fileLoader.ts
+++ src/utils/fileLoader.ts
@@ -1,7 +1,7 @@
 import { FileData } from '../types';
 
 export async function loadMarkdownFiles(): Promise<FileData[]> {
-  const markdownModules = import.meta.glob('../data/files/*.md', { as: 'raw', eager: true });
+  const markdownModules = import.meta.glob('../data/files/*.md', { as: '?raw', eager: true });
   
   return Object.entries(markdownModules).map(([path, content]) => ({
     name: path.split('/').pop() || '',
